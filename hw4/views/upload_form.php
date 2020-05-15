f32d3948a71cb65ffdaee0545344f92ea43a3051
KrylovArseny <arsenjfr@gmail.com>
2020-05-15 19:16:54 +0300

    ДЗ 4

---
+++ hw4/views/upload_form.php
@@ -0,0 +1,16 @@
+<style>
+    img {
+        max-width: 250px;
+    }
+</style>
+<h2>Form</h2>
+<form action="" enctype="multipart/form-data" method="post">
+    <input type="file" name="my_file">
+    <input type="submit">
+</form>
+<h2>Galer</h2>
+
+
+
+
+
