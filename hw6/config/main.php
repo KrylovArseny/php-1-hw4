1819ff8c375f3cd29b13a0917859061982649425
KrylovArseny <46763160+KrylovArseny@users.noreply.github.com>
2020-05-15 19:09:39 +0300

    Initial commit

---
+++ hw6/config/main.php
@@ -0,0 +1,8 @@
+
+<?php
+define("DOCUMENT_ROOT", __DIR__ . "/../");
+define("CONF_DIR", DOCUMENT_ROOT . "config/");
+define("VIEWS_DIR", DOCUMENT_ROOT . "views/");
+define("PUBLIC_DIR", DOCUMENT_ROOT . "public/");
+define("VENDOR_DIR", DOCUMENT_ROOT . "vendor/");
+define("ENGINE_DIR", DOCUMENT_ROOT . "engine/");
---
+++ hw6/public/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '\..\config\main.php';
+require_once VIEWS_DIR . 'calk.php';
+require_once ENGINE_DIR . 'post_calk.php';
+
+getArg();
+
+require_once VIEWS_DIR . 'calk2.php';
+require_once ENGINE_DIR . 'post_calk2.php';
+getArg2();
