71132f54c589dc4ab91afe3e7ee527e04a9a1447
KrylovArseny <arsenjfr@gmail.com>
2020-05-19 15:56:57 +0300

    hw

---
+++ hw6/engine/post_calk2.php
@@ -0,0 +1,41 @@
+<?php
+
+function getArg2 (){
+
+
+    if ($_POST['arg1'] && $_POST['arg1'] != '0') {
+        $arg1 = (int)strip_tags($_POST['arg1']);
+    } else{
+        $arg1 = (int)0;
+    };
+    if ($_POST['arg2']&& $_POST['arg2'] != '0') {
+        $arg2 = (int)strip_tags($_POST['arg2']);
+    }else{
+        $arg2 = (int)0;
+    };
+
+
+    if($_POST['+']) {
+        $res = $arg1 + $arg2;
+        echo "</p>{$arg1}+{$arg2}=<b>{$res}</b></p>";
+    };
+
+    if($_POST['-']) {
+        $res = $arg1- $arg2;
+        echo "</p>{$arg1}-{$arg2}=<b>{$res}</b></p>";
+    };
+
+    if($_POST['*'] == '*') {
+        $res = $arg1 * $arg2;
+        echo "</p>{$arg1}*{$arg2}=<b>{$res}</b></p>";
+    };
+
+    if($_POST['/']&& $arg2 == 0) {
+        echo 'На ноль делить недьзя';
+    } elseif ($_POST['/']){
+        $res = $arg1 / $arg2;
+        echo "</p>{$arg1}/{$arg2}=<b>{$res}</b></p>";
+    };
+
+
+}
---
+++ hw6/views/calk2.php
@@ -0,0 +1,10 @@
+<h2>Calk2</h2>
+<form action="" method="post">
+    <input type="text" name="arg1">
+    <input type="text" name="arg2"><br>
+
+    <input name="+" type="submit" value="+">
+    <input name="-" type="submit" value="-">
+    <input name="*" type="submit" value="*">
+    <input name="/" type="submit" value="/">
+</form>
